10758d6fc46d060c67eb2466e3dcb13f73dd3714
coledockter <cdockter@telegeography.com>
2025-01-10 16:03:00 -0600

    Adding data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-
+from datetime import datetime
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -10,8 +11,15 @@ def home():
 # <station> is a special syntax flask uses to pass in params
 @app.route('/api/v1/<station>/<date>')
 def api_func(station, date):
-    df = pandas.read_csv("")
-    temperature = df.station(date)
+    # Read in data from csv using API inputs.
+    filename = f"weather_data/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+
+    # Format our date into yyyy-mm-dd without time.
+    formatted_date = datetime.strptime(date, "%Y%m%d").date()
+
+    # Grab temperature from dataframe
+    temperature = df.loc[df['    DATE'] == f'{formatted_date}']['   TG'].squeeze() / 10
     return {"station": station, "date": date, "temperature": temperature}
 
-app.run(debug=True)
+app.run(debug=True, port=5000)
